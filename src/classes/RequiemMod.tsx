02cccb602a74a8ba5f56d9b0a0794edf498ff1d5
Steven <Briggs.steven369@gmail.com>
2024-08-08 20:38:44 -0700

    Add RequiemMod class and initialize requiems array
    Introduced a new class RequiemMod to manage requiem names. Also created a static list of RequiemMod instances for predefined requiems. This setup lays the groundwork for further expansion of requiem functionalities.

---
+++ src/classes/RequiemMod.tsx
@@ -0,0 +1,12 @@
+class RequiemMod {
+    public requiemName: string;
+    // public requiemRelic: string;
+    // public chargeCount: number;
+
+    constructor(requiemName: string) {
+        this.requiemName = requiemName;
+        // this.requiemRelic = requiemRelic;
+    }
+}
+
+export default RequiemMod;
---
+++ src/static/requiems.tsx
@@ -0,0 +1,13 @@
+import RequiemMod from "../classes/RequiemMod.tsx";
+
+export var requiems: Array<RequiemMod> = [
+    new RequiemMod("Oul"),
+    new RequiemMod("Vome"),
+    new RequiemMod("Xata"),
+    new RequiemMod("Fass"),
+    new RequiemMod("Jahu"),
+    new RequiemMod("Khra"),
+    new RequiemMod("Netra"),
+    new RequiemMod("Ris"),
+    new RequiemMod("Lohk"),
+];
